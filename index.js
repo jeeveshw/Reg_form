76c28e50ec88c6ccb0834aa4b717cbbfab4f27aa
jeevesh <28839382+jeeveshw@users.noreply.github.com>
2023-03-06 19:27:36 +0530

    popup  commit

--- index.js
+++ index.js
@@ -32,5 +32,13 @@ function validation (){
             document.getElementById("result").innerHTML="Password must be at least 6 characters";
             return false;
                 }
+    else if (document.Formfill.Password.value == document.Formfill.cPassword.value){
+                    popup.classList.add("open-slide")
+                    return false;
+            }
 
+}
+var popup = document.getElementById("popup");
+function CloseSlide(){
+    popup.classList.remove("open-slide");
 }
