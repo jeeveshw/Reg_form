83fafc655ebaa2d133563ac7f4709b01cd286f6d
jeevesh <28839382+jeeveshw@users.noreply.github.com>
2023-03-06 16:44:28 +0530

    java script commit

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ function validation (){
         return false;
 
     }
-    else{
-        (document.Formfill.Username.value.length <6){
+    else if (document.Formfill.Username.value.length<6){
             document.getElementById("result").innerHTML="At least six letters required";
             return false;
     
+    }
+    else if (document.Formfill.Email.value == ""){
+        document.getElementById("result").innerHTML="Enter email ID ";
+        return false;
+
+            }
+    else if (document.Formfill.Password.value == ""){
+                document.getElementById("result").innerHTML="Enter Password ";
+                return false;
     }
 }
